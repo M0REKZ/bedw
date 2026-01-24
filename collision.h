fe196511784e94e7fffeb182a612c1385ccfb31a
+KZ <60852359+M0REKZ@users.noreply.github.com>
2026-01-24 20:39:59 -0300

    add a missing include

--- collision.h
+++ collision.h
@@ -5,6 +5,7 @@
 
 #include <raylib.h>
 #include <game.h>
+#include <cstddef>
 
 bool IntersectLines(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, Vector2 &intersection);
 float GetAngleBetweenPoints(Vector2 p1, Vector2 p2);
